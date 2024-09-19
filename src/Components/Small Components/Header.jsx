0d20952b943cd0c0b3cb1107c4153533a7e9c206
Oindil Golder <oindilgolder303@gmail.com>
2024-09-19 18:18:02 +0530

    added

--- src/Components/Small Components/Header.jsx
+++ src/Components/Small Components/Header.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-        <header className='bg-gradient-to-r from-amber-200 to-yellow-500'>
+        <header className='bg-gradient-to-r from-blue-800 to-black py-1'>
             <div className='max-w-7xl mx-auto text-center'>
-                <span className='font-inter'>Welcome to My World.{" "}
-                    <a href="https://www.linkedin.com/in/oindil-golder" target="_blank" className='underline'>Let's Connect</a>
+                <span className='font-inter text-white text-sm'>Welcome to My World.{" "}
+                    <a href="https://www.linkedin.com/in/oindil-golder" target="_blank" className='underline text-white'>Let's Connect</a>
                 </span>
             </div>
         </header>
