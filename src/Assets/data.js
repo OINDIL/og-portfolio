9c7bc29f03055a66bf1580af96e072d56fe505b9
Oindil Golder <91556756+OINDIL@users.noreply.github.com>
2024-12-14 20:53:10 +0530

    update: full stack added

--- src/Assets/data.js
+++ src/Assets/data.js
@@ -1,5 +1,5 @@
 export const experience = [
-    { domain: 'Front end developer & Teacher', company: 'Biswas Company', link: 'https://biswascompany.com', years: '1+ years', desc: 'Building in house projects and teaching 500+ students online.' },
+    { domain: 'Full Stack Dev & Teacher', company: 'Biswas Company', link: 'https://biswascompany.com', years: '1+ years', desc: 'Building in house projects and teaching 500+ students online.' },
     { domain: 'Lead developer & Co-founder', company: 'Digionestep', link: 'https://digionestep.com', years: '4 months', desc: 'Co-founder of this company where I look after the client projects as well as my team members.' },
 ]
 
